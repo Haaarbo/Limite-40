8d9dffca585146bb6daab533d25c0f007383bdd8
Rui Neto <144974861+RiuTone@users.noreply.github.com>
2025-09-16 22:51:04 -0400

    Implementação da lógica do slider
    Abublé abublé

--- limite-40/testes/path2d/path2d.gd
+++ limite-40/testes/path2d/path2d.gd
@@ -1,7 +1,31 @@
 extends Path2D
 
+var nunPessoas: int = 0
+var nunCarros: int = 0
+
+func aplyValue(cars: int, people: int):
+	if cars != 0:
+		nunCarros += cars
+		$nunCarros.text = str(nunCarros)
+	
+	elif people != 0:
+		nunPessoas += people
+		$nunPessoas.text = str(nunPessoas)
+		
+	var calc: int = (nunCarros - nunPessoas) 
+	
+	if calc > 50:
+		calc = 50
+	if calc < -50:
+		calc = -50
+	
+	$PathFollow2D/rel.text = str(calc)
+	$PathFollow2D.progress_ratio = 0.5 - (float(calc) / 100)
+
 func _process(delta: float) -> void:
+
 	if Input.is_action_pressed("Play"):
-		$PathFollow2D.progress += 10
+		aplyValue(0, 1)
+
 	elif Input.is_action_pressed("Dissplay"):
-		$PathFollow2D.progress -= 10
+		aplyValue(1,0)
